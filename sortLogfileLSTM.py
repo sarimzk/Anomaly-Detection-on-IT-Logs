9656ac9d867cb2e5352d73b3bf8911d1bf70ae89
itsmevikrant <vikrant@buffalo.edu>
2018-11-06 20:38:23 -0500

    Added Sorting Method
    Added file to sort the logfile by two methods. Method 1: split Time column in two date and time columns and then sort using date column. Tme taken for 5 Million Log lines 2M31 Secs
    Method 2 sort the time column
    Time taken for 5 million log lines 1M 79 secs

---
+++ sortLogfileLSTM.py
@@ -0,0 +1,65 @@
+import csv
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+import datetime as dt
+from matplotlib import style
+from sklearn import svm, metrics
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+import datetime
+import dateutil.parser
+from datetime import datetime
+from dateutil.parser import parse
+import datetime
+import dateutil.parser
+import re
+import time
+#re.compile('<title>(.*)</title>')
+#%matplotlib inline
+#Path to CSV Files
+start = time.time()
+
+path = "/Users/vikrant/jupyternotebooks/"
+os.chdir(path)
+print("pathset")
+#Create DATAFRAME
+start1 = time.time()
+df1=pd.read_csv('logfilemarch.csv', na_filter=False)
+end1= time.time()
+print("Took %f ms in reading complete logfile" % ((end1 - start1) * 1000.0))
+
+df2= df1.iloc[0:10000000]
+df2.columns=["Time", "Machine", "Daemon", "Log_Message"]
+
+print("readfile 5 m logs")
+#df2=df1
+#Create Smaller Datasets
+dft=df2
+dft2=df2
+print("Smaller datasets created")
+#Set Columns
+#dft.columns=["Time", "Machine", "Daemon", "Log_Message"]
+#dft2.columns=["Time", "Machine", "Daemon", "Log_Message"]
+
+#SORTING USING METHOD1
+start2 = time.time()
+dft[['date', 'time']] = dft2.Time.str.split(' ', expand = True)
+dft.sort_values("date", inplace=True)
+dft.to_csv('logfileSorted2extraColumns.csv')
+print("CSV File dft.csv created")
+end2= time.time()
+print("Took %f ms in creating csv method1" % ((end2 - start2) * 1000.0))
+
+
+#SORTING using method2
+start3 = time.time()
+dft3 = dft2.sort_values(by='Time',ascending=True)
+dft3.to_csv('logfileSorted.csv')
+print("CSV File dft3.csv created")
+end3= time.time()
+print("Took %f ms in creating csv method1" % ((end3 - start3) * 1000.0))
+end = time.time()
+print("Took %f ms in reading and running two methods" % ((end - start) * 1000.0))
